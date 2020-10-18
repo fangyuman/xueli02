7fbfcb557395f4c5c89cb93fcebeb6064eb2b146
方誉蔓 <45160587+fangyuman@users.noreply.github.com>
2020-10-18 17:26:19 +0800

    Add files via upload

---
+++ precache-manifest.f42d1860300cf2bf35432f87633faeea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0749b6d8016345525f1b114112a975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4194b2169ea881a7353b",
+    "url": "/static/css/2.3786585f.chunk.css"
+  },
+  {
+    "revision": "4678636cf4ecef80b47a",
+    "url": "/static/css/main.92a3ebff.chunk.css"
+  },
+  {
+    "revision": "4194b2169ea881a7353b",
+    "url": "/static/js/2.365b89cc.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.365b89cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4678636cf4ecef80b47a",
+    "url": "/static/js/main.de352f25.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
